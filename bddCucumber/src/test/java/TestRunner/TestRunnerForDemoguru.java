89f562a6c991a33eb372b69ac5635b44323ca948
Mayankdaiya <mayankdaiya03@gmail.com>
2025-08-25 17:18:26 +0530

    changes added

--- bddCucumber/src/test/java/TestRunner/TestRunnerForDemoguru.java
+++ bddCucumber/src/test/java/TestRunner/TestRunnerForDemoguru.java
@@ -1,16 +1,16 @@
 package TestRunner;
 
 import io.cucumber.junit.Cucumber;
-import io.cucumber.junit.CucumberOptions;
+import io.cucumber.testng.CucumberOptions;
 import io.cucumber.testng.AbstractTestNGCucumberTests;
-import org.junit.runner.RunWith;
+//import org.junit.runner.RunWith;
 
-@RunWith(Cucumber.class)
+//@RunWith(Cucumber.class)
 @CucumberOptions(
         features = "src/test/resources/Features/demoguru.feature",
         glue = {"StepDefinitions.demoguru99", "Hooks"},   // <--- match your step definition package
         plugin = {"pretty", "html:target/cucumber-reports"},
         monochrome = true
 )
-public class TestRunnerForDemoguru{
+public class TestRunnerForDemoguru extends AbstractTestNGCucumberTests{
 }
